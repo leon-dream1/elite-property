3bfa38e1e4c2d938c8d4a7b8d572ce3416588a08
leon-dream1~ <nahidulislam019045@gmail.com>
2024-06-02 20:25:46 +0600

    Page not found added

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,11 +3,13 @@ import MainLayout from "../Layout/MainLayout/MainLayout";
 import Home from "../pages/Home/Home/Home";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
+import PageNotFound from "../pages/PageNotFound/PageNotFound";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
+    errorElement: <PageNotFound />,
     children: [
       {
         index: true,
