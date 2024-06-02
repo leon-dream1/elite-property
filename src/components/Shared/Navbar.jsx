10828b2cf155f686e73d37ff133f102d3a47f619
leon-dream1~ <nahidulislam019045@gmail.com>
2024-06-02 14:29:48 +0600

    Basic setup done

---
+++ src/components/Shared/Navbar.jsx
@@ -0,0 +1,214 @@
+import { Link, NavLink } from "react-router-dom";
+// import userIcon from "/user.png";
+// import { Tooltip } from "react-tooltip";
+
+const Navbar = () => {
+  //   const navigate = useNavigate();
+
+  return (
+    <div className="bg-[#333333] lg:p-2">
+      <div className="container mx-auto">
+        <div className="navbar">
+          <div className="navbar-start">
+            <div className="dropdown text-white">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[10] p-2 shadow bg-base-100 rounded-box w-52 space-y-2 lg:space-y-0"
+              >
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-[20px] font-merriweather text-[#FFAC41] font-bold"
+                      : "text-[20px] text-[#131313] font-merriweather font-medium"
+                  }
+                  to="/"
+                >
+                  Home
+                </NavLink>
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-[20px] font-merriweather text-[#FFAC41] font-bold"
+                      : "text-[20px] text-[#131313] font-merriweather font-medium"
+                  }
+                  to="/allRoom"
+                >
+                  Rooms
+                </NavLink>
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-[20px] font-merriweather text-[#FFAC41] font-bold"
+                      : "text-[20px] text-[#131313] font-merriweather font-medium"
+                  }
+                  to="/myBooking"
+                >
+                  My Booking
+                </NavLink>
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-[20px] font-merriweather text-[#FFAC41] font-bold"
+                      : "text-[20px] text-[#131313] font-merriweather font-medium"
+                  }
+                  to="/about"
+                >
+                  About Us
+                </NavLink>
+              </ul>
+            </div>
+            <Link to="/" className="cursor-pointer">
+              {/* <img src={logo} alt="" className="w-[80%] md:w-[60%]" /> */}
+            </Link>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1 space-x-10 text-white">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-[20px] font-merriweather text-[#FFAC41] font-bold"
+                    : "text-[20px] text-white font-merriweather font-medium"
+                }
+                to="/"
+              >
+                Home
+              </NavLink>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-[20px] font-merriweather text-[#FFAC41] font-bold"
+                    : "text-[20px] text-white font-merriweather font-medium"
+                }
+                to="/allRoom"
+              >
+                Rooms
+              </NavLink>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-[20px] font-merriweather text-[#FFAC41] font-bold"
+                    : "text-[20px] text-white font-merriweather font-medium"
+                }
+                to="/myBooking"
+              >
+                My Booking
+              </NavLink>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-[20px] font-merriweather text-[#FFAC41] font-bold"
+                    : "text-[20px] text-white font-merriweather font-medium"
+                }
+                to="/about"
+              >
+                About Us
+              </NavLink>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-[20px] font-merriweather text-[#FFAC41] font-bold"
+                    : "text-[20px] text-white font-merriweather font-medium"
+                }
+                to="/contact"
+              >
+                Contact Us
+              </NavLink>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-[20px] font-merriweather text-[#FFAC41] font-bold"
+                    : "text-[20px] text-white font-merriweather font-medium"
+                }
+                to="/gallery"
+              >
+                Gallery
+              </NavLink>
+            </ul>
+          </div>
+          <div className="navbar-end">
+            {/* {user ? (
+              <div className="flex items-center space-x-1 md:space-x-4">
+                <div className="dropdown dropdown-end">
+                  <div>
+                    <div
+                      tabIndex={0}
+                      role="button"
+                      className="btn btn-ghost btn-circle avatar"
+                    >
+                      <div
+                        className="w-8 rounded-full"
+                        data-tooltip-id="my-tooltip"
+                      >
+                        <img
+                          alt="Tailwind CSS Navbar component"
+                          src={user.photoURL ? user.photoURL : { userIcon }}
+                        />
+                      </div>
+                    </div>
+                    <ul
+                      tabIndex={0}
+                      className="mt-3 z-[10] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-56"
+                    >
+                      <li>
+                        <a>{user.displayName}</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <Tooltip
+                    id="my-tooltip"
+                    content={user.displayName}
+                    offset={20}
+                    className="z-10"
+                  />
+                </div>
+                <button
+                  onClick={handleLogout}
+                  className="bg-[#ee4040] text-white text-[14px] md:text-[18px] font-merriweather font-semibold px-[10px] md:px-[35px] py-[5px] rounded-lg"
+                >
+                  Log out
+                </button>
+              </div>
+            ) : (
+              <div className="flex space-x-4 lg:space-x-6">
+                <button
+                  onClick={() => navigate("/login")}
+                  className="bg-blue-600 text-white text-[18px] px-[15px] lg:px-[25px] py-[5px] lg:py-[15px] rounded-md font-merriweather"
+                >
+                  Login
+                </button>
+                <button
+                  onClick={() => navigate("/register")}
+                  className="bg-blue-600 text-white text-[18px] px-[15px] lg:px-[25px] py-[5px] lg:py-[15px]  rounded-md font-merriweather"
+                >
+                  Register
+                </button>
+              </div>
+            )} */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="font-playfair">This is Home</h1>
+    </div>
+  );
+};
+
+export default Home;
